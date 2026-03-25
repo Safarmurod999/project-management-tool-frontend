fcc1b3fe5386d83bcea8bf461b6ecaedad02db65
Safarmurod999 <supermen0904@gmail.com>
2026-03-25 11:43:37 +0500

    feat: complete view page of teams

--- src/features/team/ui/team-view.tsx
+++ src/features/team/ui/team-view.tsx
@@ -1,3 +1,76 @@
+import { Avatar, Group, Stack, Text } from "@mantine/core";
+import { DataTable } from "mantine-datatable";
+import styles from './team-view.module.scss';
+
+const data = [
+  {
+    id: 'a1',
+    name: 'asd',
+    image: 'https://i.pravatar.cc/300',
+    email: 'asd@gmail.com',
+    role: 'admin',
+    status: 'active'
+  },
+  {
+    id: 'a2',
+    name: 'asd',
+    image: 'https://i.pravatar.cc/300',
+    email: 'ddasd',
+    role: 'admin',
+    status: 'active'
+  },
+  {
+    id: 'a3',
+    name: 'adfffffffff',
+    image: 'https://i.pravatar.cc/300',
+    email: 'ddasaaaaaaaad',
+    role: 'admin',
+    status: 'active'
+  }
+]
+
 export function TeamView() {
-  return (<div></div>)
+  return (
+    <Stack p={20} className={styles.teamView} bdrs={10}>
+      <Group justify='space-between'>
+        <Text size='xl' fw={600}>Jamoa a'zolari</Text>
+      </Group>
+
+      <DataTable
+        withRowBorders
+        striped={true}
+        highlightOnHover={true}
+        records={data || []}
+        columns={
+          [
+            {
+              accessor: 'name',
+              title: "A'zo",
+              textAlign: 'left',
+              render: (r) => (
+                <Group>
+                  <Avatar radius='sm' src={r.image} alt={r.name}/>
+                  <Stack gap='0'>
+                    <Text fw='600' tt='capitalize'>{r.name}</Text>
+                    <Text>{r.email}</Text>
+                  </Stack>
+                </Group>
+              )
+            },
+            {
+              accessor: 'role',
+              title: 'Roli',
+              textAlign: 'left'
+            },
+            {
+              accessor: 'status',
+              title: 'Holati'
+            }
+          ]
+        }
+        noRecordsText="Ma'lumotlar topilmadi"
+      />
+
+    </Stack>
+  )
 }
